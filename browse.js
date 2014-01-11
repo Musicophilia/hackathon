f5c00c349385656136735d14b3495c9dfde2f44b
Kensen Shi <kensenshi@KensenUbuntu.(none)>
2014-01-11 12:57:10 -0800

    browsing, but not yet working

---
+++ browse.js
@@ -0,0 +1,24 @@
+var client = new WindowsAzure.MobileServiceClient('https://found-found.azure-mobile.net/', 'PgYPjeVvaByElucuwGqnmaPVhkkgtf15'),
+        found_table = client.getTable('found_items');
+        lost_table = client.getTable('lost_items');
+
+window.onload = function() {
+    console.log("browse init");
+    var query = found_table.where({category:"phone"});
+    console.log(query);
+    query.read().then(function(matchedItems) {
+        console.log(matchedItems);
+        for(var i = 0; i < matchedItems.length; i++) {
+            console.log("found something: " + matchedItems[i].category + " " + matchedItems[i].location + " " + matchedItems[i].date);
+        }
+    }, handleError);
+    update_table(query);
+}
+
+function handleError(error) {
+    var text = error + (error.request ? ' - ' + error.request.status : '');
+    $('#errorlog').append($('<li>').text(text));
+}
+get = function(id) {
+    return document.getElementById(id);
+}
